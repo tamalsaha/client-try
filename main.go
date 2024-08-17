8471711f4537639280d67d922d45a31c4eab43e4
Tamal Saha <tamal@appscode.com>
2024-08-16 21:39:09 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,3 +1,70 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2/klogr"
+	"k8s.io/kubernetes/pkg/apis/core"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+)
+
+func NewClient() (client.Client, error) {
+	scheme := runtime.NewScheme()
+	_ = clientgoscheme.AddToScheme(scheme)
+
+	ctrl.SetLogger(klogr.New())
+	cfg := ctrl.GetConfigOrDie()
+	cfg.QPS = 100
+	cfg.Burst = 100
+
+	hc, err := rest.HTTPClientFor(cfg)
+	if err != nil {
+		return nil, err
+	}
+	mapper, err := apiutil.NewDynamicRESTMapper(cfg, hc)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.New(cfg, client.Options{
+		Scheme: scheme,
+		Mapper: mapper,
+		//Opts: client.WarningHandlerOptions{
+		//	SuppressWarnings:   false,
+		//	AllowDuplicateLogs: false,
+		//},
+	})
+}
+
+func main() {
+	//if err := useGeneratedClient(); err != nil {
+	//	panic(err)
+	//}
+	if err := useKubebuilderClient(); err != nil {
+		panic(err)
+	}
+}
+
+func useKubebuilderClient() error {
+	fmt.Println("Using kubebuilder client")
+	kc, err := NewClient()
+	if err != nil {
+		return err
+	}
+
+	var pglist core.NamespaceList
+	err = kc.List(context.TODO(), &pglist)
+	if err != nil {
+		return err
+	}
+	for _, db := range pglist.Items {
+		fmt.Println(client.ObjectKeyFromObject(&db))
+	}
+	return nil
+}
